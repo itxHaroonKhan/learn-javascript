034f1e4d70867b10158bfc2d9632a2b6b0302496
Code Crafter <162664649+itxHaroonKhan@users.noreply.github.com>
2025-06-28 22:39:17 -0700

    Add files via upload

---
+++ Reading field/main.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+function checkSubscription() {
+    let isSubscribed = document.getElementById('subscribe').checked;
+    document.write(isSubscribed ? "Subscribed" : "Not Subscribed");
+}
+
+
+
+
+
+function getComment() {
+    let comment = document.getElementById('commentBox').value;
+    
+    document.write(comment); // Textarea ka text print hoga
+}
+
+
+
+
+
