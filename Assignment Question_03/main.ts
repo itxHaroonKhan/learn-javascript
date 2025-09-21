2d244e3366faadd2540562603bdc6f00ee5ca3ec
Code Crafter <162664649+itxHaroonKhan@users.noreply.github.com>
2025-09-20 20:44:37 -0700

    Add files via upload

---
+++ Assignment Question_03/main.ts
@@ -0,0 +1,25 @@
+let famous_person: string = "Quaid-e-Azam Muhammad Ali Jinnah";
+let quote: string = "With faith, discipline and selfless devotion to duty, there is nothing worthwhile that you cannot achieve.";
+
+let message: string = `"${quote}" — ${famous_person}`;
+console.log(message);
+
+
+
+
+let personName: string = "\t\n   Haroon Rasheed   \n\t";
+
+console.log("With whitespace:");
+console.log(personName);   // Whitespace ke sath naam print hoga
+
+console.log("\nAfter stripping whitespace:");
+console.log(personName.trim());  // trim() se clean naam print hoga
+
+
+
+
+
+console.log(5 + 3);   // Addition = 8
+console.log(10 - 2);  // Subtraction = 8
+console.log(2 * 4);   // Multiplication = 8
+console.log(16 / 2);  // Division = 8
