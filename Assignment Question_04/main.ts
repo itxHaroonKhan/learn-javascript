15d9d9b094a3ecec137156d9853c96a210d9f3d0
Code Crafter <162664649+itxHaroonKhan@users.noreply.github.com>
2025-09-21 19:27:08 -0700

    Update main.ts

--- Assignment Question_04/main.ts
+++ Assignment Question_04/main.ts
@@ -1,4 +1,7 @@
-// Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.  Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+// Changing Guest List: You just heard that one of your guests can’t make the dinner, 
+// so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+// Start with your program from Exercise 14. Add a print statement at the end of your program stating
+// the name of the guest who can’t make it.
 
 
 
